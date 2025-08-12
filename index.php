14f32c2c5e1dfdcc40a155ef8abce7069e640973
MattBerloffe <11251103392@alunos.umc.br>
2025-08-11 21:16:52 -0300

    ficha

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ficha de empregado</title>
+</head>
+<h1>Ficha de Empregados</h1>
+<body>
+<?php 
+//wesley é burro
+
+$nome = "Wesley";
+$cargo = "Montador junior";
+$salario = 2000.00;
+$idade = 67;
+
+echo("Nome: $nome <br>
+    Cargo: $cargo <br>
+    Salário: $salario <br>
+    Idade: $idade <br>
+    ")
+?>
+</body>
+</html>
+
+<?php 
+/*//wesley é burro
+
+$nome = "wesley";
+$cargo = "montador de persiana junior";
+$salario = 2000.00;
+$idade = 67;
+
+echo("")
+
+echo("Nome: $nome <br>
+    Cargo: $cargo <br>
+    Salário: $salario <br>
+    Idade: $idade <br>
+    ")*/
+?>
